41956df89ded085d088f00c8f7d2ba56553197e7
Anthony Silva <thonycout@gmail.com>
2023-07-25 20:26:55 -0300

    router to pesquisa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import SquareHomePage from "@/components/SquareHomePage";
 import { BsCheck2Square, BsSearch } from "react-icons/bs";
 import { BiBook } from "react-icons/bi";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="flex flex-col justify-center gap-6 flex-wrap items-center h-full w-full">
-      <SquareHomePage
-        colorBgClass="bg-green-300"
-        icon={<BsSearch />}
-        name="PESQUISA"
-      />
+      <div onClick={() => router.push("/pesquisas/ent")}>
+        <SquareHomePage
+          colorBgClass="bg-green-300"
+          icon={<BsSearch />}
+          name="PESQUISA"
+        />
+      </div>
       <SquareHomePage colorBgClass="bg-red-300" icon={<BiBook />} name="QUIZ" />
       <SquareHomePage
         colorBgClass="bg-blue-300"
---
+++ pages/pesquisas/[empresa].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+function Pesquisas() {
+  const router = useRouter();
+  console.log();
+  return <div>{router.query.empresa}</div>;
+}
+
+export default Pesquisas;
