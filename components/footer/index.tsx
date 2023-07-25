bd3901c618501ad7af8a622acfba22faaacf61d9
Anthony Silva <thonycout@gmail.com>
2023-07-25 00:20:45 -0300

    footer preview

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <div className=" bg-orange-400 h-16 text-center">Footer</div>;
+  return (
+    <div className="flex justify-around items-center bg-orange-400 h-16 text-center">
+      <div className="border-r w-1/3 border-black">ICON</div>
+      <div className="border-x w-1/3 border-black">ICON</div>
+      <div className="border-l w-1/3 border-black">ICON</div>
+    </div>
+  );
 }
 
 export default Footer;
