28efe6fd82e7fa88d515a8360afcfa01d89e794e
Anthony Silva <thonycout@gmail.com>
2023-07-25 01:25:02 -0300

    footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { AiFillHome } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
-import { AiOutlineTrophy } from "react-icons/ai";
-import FooterButton from "../footerButton";
 function Footer() {
   return (
-    <div className="flex justify-around items-center bg-orange-400 h-16 text-center shadow-2xl ">
-      <FooterButton icon={<AiFillHome />} name="INICIO" />
-      <FooterButton icon={<CgProfile />} name="PERFIL" />
-      <FooterButton icon={<AiOutlineTrophy />} name="RANKING" />
+    <div className="flex justify-around items-center bg-orange-400 h-10 text-center shadow-2xl ">
+      <div className="active:bg-slate-600">Arrow Up</div>
     </div>
   );
 }
+
+//fazer uma nova interface para o footer capaz de subir o menu ao ser clicado
 
 export default Footer;
